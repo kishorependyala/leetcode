b6e56ebd80f591d72a5fb0bc15e55dde551aa51f
kishorependyala <kishorekpendyala@yahoo.com>
2021-06-18 18:36:14 -0500

    Stack Wrapper

--- src/main/java/com/kp/datastructures/StackWrapperForMax.java
+++ src/main/java/com/kp/datastructures/StackWrapperForMax.java
@@ -4,34 +4,46 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
-public class StackWrapperForMax {
+/**
+ * Enhance Stack to provide max element with O(1) complexity
+ */
+public abstract class StackWrapperForMax<V> {
 
-    private Stack<Integer> stack = new Stack<Integer>();
-    private Stack<Integer> maxStack = new Stack<Integer>();
+    private Stack<V> stack = new Stack<V>();
+    private Stack<V> maxStack = new Stack<V>();
 
-    public void push(int i) {
+    public abstract int compareTo(V value1, V value2);
+
+    public void push(V i) {
         stack.push(i);
-        if (maxStack.isEmpty() || maxStack.peek() <= i) {
+        if (maxStack.isEmpty() || compareTo(i,maxStack.peek()) <= 0) {
             maxStack.push(i);
         }
     }
 
-    public int pop() {
-        int poppedItem = stack.pop();
-        if (!maxStack.isEmpty() && maxStack.peek() == poppedItem) {
+    public V pop() {
+        V poppedItem = stack.pop();
+        if (!maxStack.isEmpty() && compareTo(maxStack.peek() , poppedItem ) == 0) {
             maxStack.pop();
         }
         return poppedItem;
     }
 
     public void printStacks(){
-        System.out.println("Stack "+stack.toString());
+        System.out.print("Stack "+stack.toString()+"\t");
         System.out.println("Max Stack"+maxStack.toString());
     }
 
     public static void main(String[] args) {
 
-        StackWrapperForMax stackWrapperForMax = new StackWrapperForMax();
+        StackWrapperForMax<Integer> stackWrapperForMax = new StackWrapperForMax<Integer>() {
+            @Override
+            public int compareTo(Integer value1, Integer value2) {
+                return value1.compareTo(value1);
+            }
+        };
+
+
         List<Integer> stackElements = Arrays.asList(5, 2, 3, 7, 7, 4);
 
         stackElements.stream().forEach(x -> {
